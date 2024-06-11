74fe48717276bcab6eceb101b4dcb62f89345c1a
Dacounter95 <146789960+germanhytsystem@users.noreply.github.com>
2024-06-11 15:02:18 -0500

    feat: Add dao/Core/CrudDao

--- src/main/java/com/ironman/dao/CategoryDao.java
+++ src/main/java/com/ironman/dao/CategoryDao.java
@@ -1,5 +1,6 @@
 package com.ironman.dao;
 
+import com.ironman.dao.Core.CrudDao;
 import com.ironman.entity.Category;
 
 import java.sql.Connection;
@@ -9,15 +10,9 @@ import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
 
-public interface CategoryDao {
+public interface CategoryDao extends CrudDao<Category,Long> {
 
-    List<Category> findAll() throws Exception;
-    Category findById(Long id) throws Exception;
-
-    int create(Category category) throws Exception;
-    int update(Long id, Category category) throws Exception;
-    void deleteById(Long id) throws Exception;
-
+    // Utiliza los métodos de CategoryDaro<>
 
     /*
     public List<Category> findAll() throws Exception {
---
+++ src/main/java/com/ironman/dao/Core/CrudDao.java
@@ -0,0 +1,11 @@
+package com.ironman.dao.Core;
+import java.util.List;
+
+public interface CrudDao<T,ID>{
+    List<T> findAll() throws Exception;
+    T findById(ID id) throws Exception;
+
+    int create(T entity) throws Exception;
+    int update(ID id, T entity) throws Exception;
+    void deleteById(ID id) throws Exception;
+}
