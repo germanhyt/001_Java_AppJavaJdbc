dbfd69d90fa9fd9dda5f2607083a0aead185e889
Dacounter95 <146789960+germanhytsystem@users.noreply.github.com>
2024-06-11 11:10:06 -0500

    feat: Add Try Resource

--- src/main/java/com/ironman/dao/CategoryDao.java
+++ src/main/java/com/ironman/dao/CategoryDao.java
@@ -10,68 +10,49 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class CategoryDao {
-    // Attributes
-    List<Category> categories= new ArrayList<>();
-    Category category;
-    String sqlQuery;
-    Connection connection;
-    ResultSet resultSet;
-    PreparedStatement preparedStatement;
 
-    public List<Category> findAll(){
+    public List<Category> findAll() {
+        // Attributes
+        List<Category> categories = new ArrayList<>();
+        Category category;
+        String sqlQuery;
+
 
         // process
-        try{
-            // Definir las columnas en el select es recomendable
-            sqlQuery = "select id, name, description , url_key , state , created_at , updated_at  from categories";
 
-            // Get connection
-            connection= new ConnectionCore().getConnection();
+        // sql query
+        sqlQuery = "select id, name, description , url_key , state , created_at , updated_at  from categories";
+        try (
+                // Get connection
+                Connection connection = new ConnectionCore().getConnection();
+                // Prepared statement
+                PreparedStatement preparedStatement = connection.prepareStatement(sqlQuery);
+                //Resultset
+                ResultSet resultSet = preparedStatement.executeQuery();
+        ) {
 
-            // Preparte statement
-            preparedStatement = connection.prepareStatement(sqlQuery);
-            resultSet = preparedStatement.executeQuery();
-
-
-            while(resultSet.next()){
-                category= new Category();
+            while (resultSet.next()) {
+                category = new Category();
                 category.setId(resultSet.getLong("id"));
                 category.setName(resultSet.getString("name"));
                 category.setUrlKey(resultSet.getString("url_key"));
                 category.setState(resultSet.getString("state"));
 
-                Timestamp createdAt= resultSet.getTimestamp("created_at");
-                if(createdAt!=null){
+                Timestamp createdAt = resultSet.getTimestamp("created_at");
+                if (createdAt != null) {
                     category.setCreateAt(createdAt.toLocalDateTime());
                 }
 
-                Timestamp updatedAt= resultSet.getTimestamp("updated_at");
-                if(updatedAt!=null){
+                Timestamp updatedAt = resultSet.getTimestamp("updated_at");
+                if (updatedAt != null) {
                     category.setUpdateTime(updatedAt.toLocalDateTime());
                 }
 
                 categories.add(category);
             }
 
-        }catch (Exception e){
-            System.out.println("CategoryDao::findAll:Error: "+e.getMessage());
-        }finally {
-             try{
-                if(resultSet!=null && !resultSet.isClosed()){
-                    resultSet.close();
-                }
-
-                if(preparedStatement!=null && !preparedStatement.isClosed()){
-                   preparedStatement.close();
-                }
-
-                if(connection!=null && !connection.isClosed()){
-                    connection.close();
-                }
-
-             }catch (Exception e){
-                 System.out.println("CategoryDao::findAll::finally "+e.getMessage());
-             }
+        } catch (Exception e) {
+            System.out.println("CategoryDao::findAll:Error: " + e.getMessage());
         }
 
         // result
